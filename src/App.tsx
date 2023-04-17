26670c62bf2c4c3b6d4b369829be31aa21a72a62
naimur2 <naimurrahaman248@gmail.com>
2023-04-17 14:06:46 +0600

    ffdg

--- src/App.tsx
+++ src/App.tsx
@@ -58,37 +58,39 @@ export default function App() {
         <div className="container mx-auto py-4 relative">
             <Role className="-mb-6 lg:-mb-3" />
             <main
-                className={`mx-6 sm:mx-8 md:mx-10 lg:mx-14 xl:mx-20 2xl:mx-[5.65rem] relative top-design main-section
-                !overflow-x-hidden
-                 circ-animation scrollbar-hide`}
+                className={`mx-6 sm:mx-8 md:mx-10 lg:mx-14 xl:mx-20 2xl:mx-[5.65rem] relative 
+                
+                 circ-animation `}
                 ref={mainSectionRef}
             >
-                <section
-                    className={`grid gap-6 md:gap-8 xl:gap-20 2xl:gap-28 pb-8 heroBg`}
-                >
-                    <Hero />
-                    <Home />
-                </section>
-                <BusinessOppurtinity />
-                <DivinePods />
-                <CulturalEthnicity />
-                <TokenEconomics />
-                <Partners />
-                <Footer />
-                <img
-                    src={topLeft}
-                    className="absolute top-0 left-0 
+                <div className="top-design main-section scrollbar-hide !overflow-x-hidden">
+                    <section
+                        className={`grid gap-6 md:gap-8 xl:gap-20 2xl:gap-28 pb-8 heroBg`}
+                    >
+                        <Hero />
+                        <Home />
+                    </section>
+                    <BusinessOppurtinity />
+                    <DivinePods />
+                    <CulturalEthnicity />
+                    <TokenEconomics />
+                    <Partners />
+                    <Footer />
+                    <img
+                        src={topLeft}
+                        className="absolute top-0 left-0 
                 w-[4rem] h-[4rem] sm:w-[5rem] sm:h-[5rem] md:w-[7rem] md:h-[7rem] lg:w-[8rem] lg:h-[8rem] 2xl:w-[10rem] 2xl:h-[10rem]
                 "
-                    alt=""
-                />
-                <img
-                    src={topLeft}
-                    className="absolute top-0 right-0 rotate-90
+                        alt=""
+                    />
+                    <img
+                        src={topLeft}
+                        className="absolute top-0 right-0 rotate-90
                     w-[4rem] h-[4rem] sm:w-[5rem] sm:h-[5rem] md:w-[7rem] md:h-[7rem] lg:w-[8rem] lg:h-[8rem] 2xl:w-[10rem] 2xl:h-[10rem]
                     "
-                    alt=""
-                />
+                        alt=""
+                    />
+                </div>
             </main>
 
             <Role className="-mt-6 lg:-mt-3" />
